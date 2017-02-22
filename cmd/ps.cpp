e82dbe4dda4ecd228649f58435acb7a201c91af4
Doug <doug@hcsw.org>
2017-02-22 13:33:58 -0500

    render

--- cmd/ps.cpp
+++ cmd/ps.cpp
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #include <iostream>
 #include <string>
@@ -44,16 +45,95 @@ void ps::process_option(int arg, int) {
     };
 }
 
+
+
+static std::string render_in_progress_header() {
+    return "EVID\tSTART\tUSER@HOSTNAME\t\tPID\tCMD";
+}
+
+
+static std::string render_command(nlohmann::json &cmd) {
+    std::string output;
+
+    for (auto &s : cmd) {
+        output += s;
+        output += " ";
+    }
+
+    return output;
+}
+
+
+static std::string render_time(uint64_t t) {
+    time_t now = ::time(nullptr);
+    t /= 1000000;
+
+    uint64_t delta = now - t;
+
+    if (delta == 0) return std::string("<1s");
+    if (delta < 60) return std::to_string(delta) + "s";
+
+    uint64_t seconds = delta % 60;
+    delta /= 60;
+
+    if (delta < 60) return std::to_string(delta) + "m" + std::to_string(seconds) + "s";
+
+    uint64_t minutes = delta % 60;
+    delta /= 60;
+
+    if (delta < 24) return std::to_string(delta) + "h" + std::to_string(minutes) + "m";
+
+    uint64_t hours = delta % 24;
+    delta /= 24;
+
+    return std::to_string(delta) + "d" + std::to_string(hours) + "h";
+}
+
+
+static std::string render_in_progress(nlohmann::json &res) {
+    std::string output;
+
+    bool has_data = !!res.count("da");
+    auto &data = res["da"];
+
+    output += res.count("ev") ? std::to_string(static_cast<uint64_t>(res["ev"])) : "?";
+    output += "\t";
+
+    output += res.count("st") ? render_time(res["st"]) : "?";
+    output += "\t";
+
+    output += has_data && data.count("user") ? data["user"] : "?";
+    output += "@";
+    output += has_data && data.count("hostname") ? data["hostname"] : "?";
+    output += "\t\t";
+
+    output += has_data && data.count("pid") ? std::to_string(static_cast<uint64_t>(data["pid"])) : "?";
+    output += "\t";
+
+    output += has_data && data.count("cmd") ? render_command(data["cmd"]) : "?";
+    output += "\t";
+
+    return output;
+}
+
+
+
 void ps::execute() {
     logp::websocket::worker ws_worker;
+
+    if (!follow) {
+        std::cout << render_in_progress_header() << std::endl;
+    }
 
     {
         logp::websocket::request r;
 
         r.op = "get";
         r.body = {{"from", 0}};
-        r.on_data = [&](nlohmann::json &resp) {
-            std::cout << "GOT: " << resp.dump() << std::endl;
+        r.on_data = [&](nlohmann::json &res) {
+            if (!follow) {
+                std::cout << render_in_progress(res) << std::endl;
+            }
         };
         r.on_finished_history = [&]{
            if (!follow) exit(0);
