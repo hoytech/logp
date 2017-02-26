c785c5e6c5b81709f7c38a5b079285e167b91939
Doug <doug@hcsw.org>
2017-02-25 22:55:48 -0500

    np

--- cmd/ping.cpp
+++ cmd/ping.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 
 #include <iostream>
 #include <string>
@@ -54,7 +55,7 @@ void ping::process_option(int arg, int, char *optarg) {
 
 
 
-std::string format_ms(uint64_t time_us) {
+std::string format_ms(int64_t time_us) {
     double d = time_us / 1000.0;
     char buf[100];
 
@@ -63,6 +64,38 @@ std::string format_ms(uint64_t time_us) {
     return std::string(buf);
 }
 
+
+std::string format_time(uint64_t time_us) {
+    time_t secs = time_us / 1000000;
+    uint64_t usecs = time_us - ((uint64_t)secs * 1000000);
+    char buf[100];
+    struct tm tm;
+
+    localtime_r(&secs, &tm);
+
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S.", &tm);
+
+    std::string output(buf);
+
+    snprintf(buf, sizeof(buf), "%06g ", (double)usecs);
+    output += buf;
+
+    strftime(buf, sizeof(buf), "%Z", &tm);
+    output += buf;
+
+    return output;
+}
+
+
+std::string format_ping_delta(uint64_t local_start, uint64_t local_end, uint64_t remote) {
+    uint64_t d = local_end - local_start;
+    d /= 2;
+    d += local_start;
+    int64_t delta = (int64_t)remote - d;
+    std::string output = format_ms(delta) + "ms";
+    if (std::abs(delta) >= 1000000) return logp::util::colour_red(output);
+    return output;
+}
 
 
 void ping::execute() {
@@ -91,8 +124,7 @@ void ping::execute() {
                 uint64_t end = logp::util::curr_time();
 
                 uint64_t server_time = res["time"];
-                std::cout << "PONG " << format_ms(end-start) << "ms " << server_time << std::endl;
-
+                std::cout << "PONG rtt=" << format_ms(end-start) << "ms delta=" << format_ping_delta(start, end, server_time) << std::endl;
                 if (pings_left == 1) exit(0);
                 else if (pings_left > 1) pings_left--;
 
@@ -113,9 +145,11 @@ void ping::execute() {
             uint64_t prot = r["prot"];
             uint64_t time = r["time"];
 
-            std::cout << "Server info:\n";
-            std::cout << "  Protocol: " << prot << "\n";
-            std::cout << "  Time:     " << time << "\n";
+            std::cout << "Connection info:\n";
+            std::cout << "  Established in: " << format_ms(ini_end-ini_start) << "ms\n";
+            std::cout << "  Protocol:       " << prot << "\n";
+            std::cout << "  Remote time:    " << format_time(time) << "\n";
+            std::cout << "  Time delta:     " << format_ping_delta(ini_start, ini_end, time) << "\n";
             std::cout << std::endl;
 
             uint64_t perm = r["perm"];
