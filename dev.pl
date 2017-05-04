4bc5766544b47c3e2c56ff4a89131be8f8662ab9
Doug <doug@hoytech.com>
2017-05-04 13:46:48 -0400

    dist script

--- dev.pl
+++ dev.pl
@@ -10,14 +10,15 @@ my $cmd = shift || die "need command";
 
 my $version;
 
-if ($cmd =~ /^dist-/) {
+if ($cmd =~ /^dist-/ || $cmd =~ /^upload-/) {
   $version = LogPeriodic::BuildLib::get_version('logp');
   ok_to_release($version);
 }
 
 if ($cmd eq 'dist-linux') {
-  sys(q{make clean});
+  sys(q{make clean}) unless $ENV{NO_CLEAN};
   sys(q{make -j 4 XCXXFLAGS="-DSSL_PIN_CAFILE" XLDFLAGS="-Wl,-rpath=/usr/logp/lib/ -static-libgcc -static-libstdc++ -s" });
+  sys(q{strip logp logp_preload.so});
 
   LogPeriodic::BuildLib::fpm({
     types => [qw/ deb rpm /],
@@ -36,12 +37,22 @@ if ($cmd eq 'dist-linux') {
     /],
     description => 'Command-line client for Log Periodic',
   });
+
+  sys(q{apt-ftparchive packages dist/ > dist/Packages});
+  sys(q{apt-ftparchive release dist/ > dist/Release});
+  sys(q{gpg -ab < dist/Release > dist/Release.gpg});
+  sys(q{gpg --export -a 'Doug Hoyte' > dist/logp-gpg-key.public});
+  sys(q{createrepo --database dist/});
 } elsif ($cmd eq 'dist-macos') {
   sys(q{make clean});
   ## FIXME: figure out how to pin CAFILE for Mac OS
   sys(q{ make -j 4 XCXXFLAGS="-I/usr/local/opt/openssl/include" XLDFLAGS="-L/usr/local/opt/openssl/lib" });
   mkdir('dist');
   sys(qq{ tar -czf dist/logp-$version.bottle.tar.gz logp });
+} elsif ($cmd eq 'upload-s3') {
+  sys(q{aws s3 sync --delete dist/ s3://logp/});
+  sys(qq{aws s3 cp s3://logp/logp_${version}_amd64.deb s3://logp/logp_latest_amd64.deb});
+  sys(qq{aws s3 cp s3://logp/logp-${version}-1.x86_64.rpm s3://logp/logp-latest.x86_64.rpm});
 } else {
   die "unknown command: $cmd";
 }
